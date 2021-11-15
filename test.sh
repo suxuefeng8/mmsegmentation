4373de3e3a07a1cf4a3d413179a1709f3cb00cff
suxuefeng <suxuefeng@sogou-inc.com>
2021-11-15 18:44:12 +0800

    commit

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-model="celeba_mask_ocr_1024"
+#model="celeba_mask_ocr_1024"
 #model="celeba_mask_swin_22k"
-#model="celeba_mask_swin_large_22k"
+model="celeba_mask_swin_large_22k"
 #model="celeba_mask_segformer"
 
-test_epoch="1"
+test_epoch="25"
 checkpoint="work_dirs/"${model}"/epoch_"${test_epoch}".pth"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=. python -m torch.distributed.launch --nproc_per_node=4 --master_port=36500 tools/test.py "configs/celeba_mask/"${model}.py $checkpoint --eval mIoU --launcher pytorch >test_${model}.log 2>&1 &
